e5ebbb0cfdafd2b38c22df4d08173dac18ec90aa
student <fiepaz03@gmail.com>
2022-02-08 19:42:48 +0500

    initial

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val view = findViewById<TextView>(R.id.testView)
+        view.text = "Привет"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var student = Student("Ivan", 2)
+        print(student.name)
+        print(student.toString())
     }
+}
+
+data class Student(var name : String, var course : Int = 1) {
+    override fun toString(): String {
+        return "My name is $name and I'm in my $course year"
+    }
+
 }
