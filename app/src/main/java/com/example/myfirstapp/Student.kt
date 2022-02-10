801b44ffb4d24f70d3e993ebb513d05f97159d95
Sofya <fiepaz03@gmail.com>
2022-02-10 21:24:40 +0500

    щупаем котлин

--- app/src/main/java/com/example/myfirstapp/Student.kt
+++ app/src/main/java/com/example/myfirstapp/Student.kt
@@ -4,14 +4,16 @@ data class Student(var name : String, var course : Int = 1) {
     override fun toString(): String {
         return "Меня зовут $name и я учусь на $course${endingOfCourseNum(course)} курсе"
     }
-}
 
-fun endingOfCourseNum(num : Int) : String { //он лежит здесь, потому что так в котлине реализована статика, лол
-    return if (num in 1..2 || num in 4..6) {
-        "-ом"
-    } else if (num == 3) {
-        "-ем"
-    } else {
-        throw Exception("Некорректные данные") // не дает выбросить эксепшн в начале, пока хз почему
+    private fun endingOfCourseNum(num : Int) : String {
+        if (num in 1..6) {
+            if (num != 3) {
+                return "-ом"
+            }
+            else if (num == 3) {
+                return "-ем"
+            }
+        }
+        throw Exception("Некорректные данные")
     }
 }
