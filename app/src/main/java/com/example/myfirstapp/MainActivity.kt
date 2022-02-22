b6d9c38ec219df00c28a7262ce5e0ac812d9bdb3
Sofya <fiepaz03@gmail.com>
2022-02-22 13:13:48 +0500

    Add new Activity with transmission

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,17 +1,42 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val student = Student("Софья", 3)
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val view = findViewById<TextView>(R.id.testView)
+        /*val view = findViewById<TextView>(R.id.textView)*/
         /*view.text = "Привет"*/
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var student = Student("Софья", 3)
+        textView = findViewById(R.id.textView)
         print(student.name)
         print(student.toString())
     }
+
+    fun studentHello(view: View) {
+        val myStudent = Toast.makeText(this, "Hello, ${student.name}", Toast.LENGTH_SHORT)
+        myStudent.show()
+    }
+
+    fun countMe(view: View) {
+        var count: Int = Integer.parseInt(textView.text.toString())
+        count++
+        textView.text = count.toString()
+    }
+
+    fun randomMe(view: View) {
+        val randomIntent = Intent(this, SecondActivity::class.java)
+        var count: Int = Integer.parseInt(textView.text.toString())
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+        startActivity(randomIntent)
+    }
+
 }
---
+++ app/src/main/java/com/example/myfirstapp/SecondActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import java.util.*
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var random : TextView
+    private lateinit var label : TextView
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        random = findViewById(R.id.textView_random)
+        label = findViewById(R.id.textView_label)
+        showRandomNumber()
+    }
+
+    fun showRandomNumber() {
+        val random = Random()
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+        var randomInt = 0
+        if (count > 0) {
+            randomInt = random.nextInt(count + 1)
+        }
+        this.random.text = Integer.toString(randomInt)
+        label.text = getString(R.string.random_heading, count)
+    }
+}
